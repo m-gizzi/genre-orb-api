d3fcaa7064dac0045c8e43de860f120d2989f67f
Matthew <matthewgizzi@verizon.net>
2026-06-30 19:48:58 -0500

    Add specs

--- spec/services/spotify_oauth_service_spec.rb
+++ spec/services/spotify_oauth_service_spec.rb
@@ -3,28 +3,7 @@
 require "rails_helper"
 
 RSpec.describe SpotifyOauthService do
-  let(:auth_hash) do
-    {
-      "uid" => "spotify_user_123",
-      "info" => {
-        "name" => "Test User",
-        "email" => "spotify@example.com",
-        "images" => [{ "url" => "https://example.com/avatar.jpg" }],
-        "country" => "US",
-        "product" => "premium",
-      },
-      "credentials" => {
-        "token" => "access_token_abc",
-        "refresh_token" => "refresh_token_xyz",
-        "expires_at" => 1.hour.from_now.to_i,
-      },
-      "extra" => {
-        "raw_info" => {
-          "external_urls" => { "spotify" => "https://open.spotify.com/user/spotify_user_123" },
-        },
-      },
-    }
-  end
+  let(:auth_hash) { spotify_omniauth_hash }
 
   describe "#call" do
     subject(:result) { described_class.new(current_user, auth_hash).call }
