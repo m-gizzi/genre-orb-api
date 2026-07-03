b79ffda1ecc9360fa34abfa6bc69d47c57613454
Matthew <matthewgizzi@verizon.net>
2026-07-02 21:18:05 -0500

    Rubocop fix

--- config/initializers/active_record_encryption.rb
+++ config/initializers/active_record_encryption.rb
@@ -8,6 +8,10 @@
 
 if ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"].present?
   Rails.application.config.active_record.encryption.primary_key = ENV["ACTIVE_RECORD_ENCRYPTION_PRIMARY_KEY"]
-  Rails.application.config.active_record.encryption.deterministic_key = ENV["ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY"]
-  Rails.application.config.active_record.encryption.key_derivation_salt = ENV["ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT"]
+  Rails.application.config.active_record.encryption.deterministic_key = ENV.fetch(
+    "ACTIVE_RECORD_ENCRYPTION_DETERMINISTIC_KEY", nil,
+  )
+  Rails.application.config.active_record.encryption.key_derivation_salt = ENV.fetch(
+    "ACTIVE_RECORD_ENCRYPTION_KEY_DERIVATION_SALT", nil,
+  )
 end
