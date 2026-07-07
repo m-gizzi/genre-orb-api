c743dd2fa6a8bcdcc43dc88fe867bfd53380a3ac
Matthew <matthewgizzi@verizon.net>
2026-07-06 22:03:44 -0500

    Squash migrations

--- db/migrate/20260706233021_add_snapshot_id_to_playlist_versions.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddSnapshotIdToPlaylistVersions < ActiveRecord::Migration[8.1]
-  def change
-    add_column :playlist_versions, :snapshot_id, :string
-  end
-end
--- db/migrate/20260707002502_remove_snapshot_id_from_playlist_versions.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveSnapshotIdFromPlaylistVersions < ActiveRecord::Migration[8.1]
-  def change
-    safety_assured { remove_column :playlist_versions, :snapshot_id, :string }
-  end
-end
