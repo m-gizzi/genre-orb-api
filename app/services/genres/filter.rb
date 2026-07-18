40627d8356f38c0d9a62b8d4202dfd16da2887f3
Matthew <matthewgizzi@verizon.net>
2026-07-18 17:22:12 -0500

    Rubocop fix

--- app/services/genres/filter.rb
+++ app/services/genres/filter.rb
@@ -10,6 +10,21 @@ module Genres
     DEFAULT_SORT = "name"
     SORT_NULLS = :none
 
+    TRACK_COUNTS_JOIN = <<~SQL.squish
+      INNER JOIN (
+        SELECT track_genres.genre_id, COUNT(DISTINCT track_genres.track_id) AS library_track_count
+        FROM track_genres
+        WHERE track_genres.track_id IN (
+          SELECT playlist_version_tracks.track_id FROM playlist_version_tracks
+          WHERE playlist_version_tracks.playlist_version_id IN (
+            SELECT playlists.current_version_id FROM playlists
+            WHERE playlists.user_id = ? AND playlists.current_version_id IS NOT NULL
+          )
+        )
+        GROUP BY track_genres.genre_id
+      ) library_track_counts ON library_track_counts.genre_id = genres.id
+    SQL
+
     def call
       relation = search(base_relation, Genre.arel_table[:name])
       relation.order(*sort.terms)
@@ -24,22 +39,7 @@ module Genres
     end
 
     def library_track_counts_join
-      ActiveRecord::Base.sanitize_sql_array(
-        [
-          "INNER JOIN (" \
-          "SELECT track_genres.genre_id, COUNT(DISTINCT track_genres.track_id) AS library_track_count " \
-          "FROM track_genres " \
-          "WHERE track_genres.track_id IN (" \
-          "SELECT playlist_version_tracks.track_id FROM playlist_version_tracks " \
-          "WHERE playlist_version_tracks.playlist_version_id IN (" \
-          "SELECT playlists.current_version_id FROM playlists " \
-          "WHERE playlists.user_id = ? AND playlists.current_version_id IS NOT NULL" \
-          ")) " \
-          "GROUP BY track_genres.genre_id" \
-          ") library_track_counts ON library_track_counts.genre_id = genres.id",
-          user.id,
-        ],
-      )
+      ActiveRecord::Base.sanitize_sql_array([TRACK_COUNTS_JOIN, user.id])
     end
   end
 end
